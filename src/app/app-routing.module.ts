8f2e16800a9edf74cc7a198642e41dceebe933e7
Ricardo.Maia <ana07785>
2021-09-14 11:16:59 -0300

    Teste full stack

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,56 @@
+import { HomeComponent } from './home/home.component';
+import { AuthGuardService } from './_guards/auth-guard.service';
+import { AdminComponent } from './admin/admin.component';
+import { CreateUserComponent } from './user/create-user/create-user.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UpdateUserComponent } from './user/update-user/update-user.component';
+import { DetailsUserComponent } from './user/details-user/details-user.component';
+import { Role } from './_models/role';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent,
+    canActivate: [AuthGuardService]
+  },
+  { 
+    path: 'admin', 
+    component: AdminComponent, 
+    canActivate: [AuthGuardService], 
+    data: { roles: [Role.Admin] } 
+},
+  {
+    path:'add',
+    component: CreateUserComponent
+  },
+  {
+    path:'list',
+    component: UserListComponent
+  },
+  {
+    path:'update/:id',
+    component: UpdateUserComponent
+  },
+  {
+    path:'details/:id',
+    component: DetailsUserComponent
+  },
+  {
+    path:'login',
+    component: LoginComponent,
+    
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+  
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,56 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { JwtInterceptorService } from './_helpers/jwt-interceptor.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { CreateUserComponent } from './user/create-user/create-user.component';
+import { UpdateUserComponent } from './user/update-user/update-user.component';
+import { DetailsUserComponent } from './user/details-user/details-user.component';
+
+import { TextMaskModule } from 'angular2-text-mask';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { NgBrazil } from 'ng-brazil';
+import { ValidatorsModule } from 'ngx-validators';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { ErrorInterceptorService } from './_helpers/error-interceptor.service';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    CreateUserComponent,
+    UpdateUserComponent,
+    DetailsUserComponent,
+    HomeComponent,
+    AdminComponent,
+    LoginComponent, 
   ],
   imports: [
+    //angular
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    AppRoutingModule,
+    CommonModule,
+    RouterModule,
+
+    TextMaskModule,
+    NgBrazil,
+
+    ValidatorsModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptorService, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptorService, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
